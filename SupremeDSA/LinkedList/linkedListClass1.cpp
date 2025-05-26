04ec6529d82dc6b0b3ca1ba18faf83d091ecbbea
suraj kumar <surajksharma2304@gmail.com>
2025-05-26 09:50:23 +0530

    linked list class 1

--- SupremeDSA/LinkedList/linkedListClass1.cpp
+++ SupremeDSA/LinkedList/linkedListClass1.cpp
@@ -1,17 +1,66 @@
 #include<iostream>
 using namespace std;
 
-class node{
-public:
-    int data;
-    node * next;
+class Node{
+    public:
+        int data;
+        Node*next;
 
-    node(int value){{
-        this->data = value;
-        this->next = NULL;
-    }}
+        Node(int data){
+            this->data = data;
+            this->next = NULL;
+        }
 };
 
-int main(){
-    node*first = new node();
+void printLinkedlist(Node * &head){
+    Node * temp = head;
+    while(temp!= NULL){
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL";
+    cout << endl;
 }
+Node * insertAtHead(int value, Node * &head, Node * &tail){
+    // linkedlist is empty
+    // it means we are creating first node of the linked list
+    // step1: create a new node 
+    if(head == NULL && tail == NULL){
+        Node * newnode = new Node(value);
+            head = newnode;
+            tail = newnode;
+    }
+    else{
+        Node * newnode = new Node(value);
+        newnode->next = head;
+        head = newnode;
+    }
+
+    return head;
+}
+
+Node * insertAtTail(int value, Node*&head, Node *&tail){
+    
+    if(head == NULL && tail == NULL){
+        Node *newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+    else{
+        Node * newnode = new Node(value);
+        tail->next = newnode;
+        tail = newnode;
+    }
+    return head;
+}
+int main(){
+    Node * head = NULL;
+    Node * tail = NULL;
+
+    insertAtHead(10,head, tail);
+    insertAtHead(20,head, tail);
+    insertAtTail(30, head, tail);
+    insertAtTail(40, head, tail);
+    printLinkedlist(head);
+}
+
