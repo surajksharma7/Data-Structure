88e1f327cf04dd78676f38c5810546331e694ad3
Suraj Kumar <surajksharma2304@gmail.com>
2025-06-10 16:25:44 +0530

    stack mega class

---
+++ Stack/megaClass.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "algorithm.h"
+
+using namespace std;
+int main()
+{
+    int arr[] = {1,2,3,4,5,6};
+    sort(arr,6);
+    return 0;
+}
--- Stack/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-    // s.push(30);
-    // s.push(90);
-    // s.push(50);
-    // s.print();
-    // s.push(50);
